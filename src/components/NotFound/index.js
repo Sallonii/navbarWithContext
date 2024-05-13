ff6ac7bba9f6fcaebac0dce4bd04ded3e631ddd8
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-05-13 12:39:03 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,6 @@
 import './index.css'
 
+import Navbar from '../Navbar'
 import ThemeContext from '../../context/ThemeContext'
 
 const NotFound = () => (
@@ -9,17 +10,20 @@ const NotFound = () => (
       const bgColor = isDarkTheme ? 'dark-home-bg' : 'light-home-bg'
       const textColor = isDarkTheme ? 'light-text-color' : 'dark-text-color'
       return (
-        <div className={`not-found-container ${bgColor}`}>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
-            alt="not found"
-            className="not-found-image"
-          />
-          <h1 className={textColor}>Lost Your Way?</h1>
-          <p className={textColor}>
-            We cannot seem to find the page you are looking for.
-          </p>
-        </div>
+        <>
+          <Navbar />
+          <div className={`not-found-container ${bgColor}`}>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+              alt="not found"
+              className="not-found-image"
+            />
+            <h1 className={textColor}>Lost Your Way?</h1>
+            <p className={textColor}>
+              We cannot seem to find the page you are looking for.
+            </p>
+          </div>
+        </>
       )
     }}
   </ThemeContext.Consumer>
