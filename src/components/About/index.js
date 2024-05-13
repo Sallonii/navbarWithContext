65057eb3ce24f82b63f763be97534bb40ef0d1ce
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-05-13 12:38:04 +0530

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import './index.css'
 
+import Navbar from '../Navbar'
 import ThemeContext from '../../context/ThemeContext'
 
 const About = () => (
@@ -9,22 +10,25 @@ const About = () => (
       const bgColor = isDarkTheme ? 'dark-home-bg' : 'light-home-bg'
       const textColor = isDarkTheme ? 'light-text-color' : 'dark-text-color'
       return (
-        <div className={`about-container ${bgColor}`}>
-          {isDarkTheme ? (
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/about-dark-img.png"
-              alt="about"
-              className="about-image"
-            />
-          ) : (
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/about-light-img.png"
-              alt="about"
-              className="about-image"
-            />
-          )}
-          <h1 className={`${textColor}`}>About</h1>
-        </div>
+        <>
+          <Navbar />
+          <div className={`about-container ${bgColor}`}>
+            {isDarkTheme ? (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/about-dark-img.png"
+                alt="about"
+                className="about-image"
+              />
+            ) : (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/about-light-img.png"
+                alt="about"
+                className="about-image"
+              />
+            )}
+            <h1 className={`${textColor}`}>About</h1>
+          </div>
+        </>
       )
     }}
   </ThemeContext.Consumer>
